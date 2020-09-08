c093ef225d071059b1a02e94d46d6111597d644f
Danielle Jenkins <git@d6e.io>
2020-09-07 22:59:50 -0700

    Implement prometheus compatible webserver.

--- src/main.ino
+++ src/main.ino
@@ -1,37 +1,52 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <WiFi.h>
 
-#define DHTPIN 23     // Digital pin connected to the DHT sensor 
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
+#define DHTPIN 23
 
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
 //#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
-
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
 
+const char* ssid = "";
+const char* password = "";
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 void setup() {
   Serial.begin(9600);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
   // Initialize device.
   dht.begin();
-  Serial.println(F("DHTxx Unified Sensor Example"));
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -64,23 +79,88 @@ void loop() {
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
+  float temperature = event.temperature;
+  if (isnan(temperature)) {
     Serial.println(F("Error reading temperature!"));
   }
   else {
     Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
+    Serial.print(temperature);
     Serial.println(F("°C"));
   }
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
+  float relative_humidity = event.relative_humidity;
+  if (isnan(relative_humidity)) {
     Serial.println(F("Error reading humidity!"));
   }
   else {
     Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
+    Serial.print(relative_humidity);
     Serial.println(F("%"));
+  }
+
+  // WIFI server
+  WiFiClient client = server.available();
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            if (header.indexOf("GET /metrics") >= 0) { // serve metrics page
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/plain; version=0.0.4; charset=utf-8");
+              client.println();
+              client.println("# HELP room_temperature The temperature from the sensor.");
+              client.println("# TYPE room_temperature gauge");
+              client.println(String("room_temperature ") + String(temperature));
+              client.println("# HELP room_relative_humidity The humidity from the sensor.");
+              client.println("# TYPE room_relative_humidity gauge");
+              client.println(String("room_relative_humidity ") + String(relative_humidity));
+            } else { // serve index page
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+
+              // Display the HTML web page
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><title>Room Environmental Sensor</title></head>");
+
+              // Web Page Heading
+              client.println("<body><h1>Room Environmental Sensor</h1><p><a href=\"/metrics\">Metrics</a></p>");
+              client.println("</body></html>");
+
+              // The HTTP response ends with another blank line
+              client.println();
+            }
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }
 
